2f06b1f09cd66837d9b81bb0751ba129b8c20396
Relampago <josemariar.l17@gmail.com>
2023-11-01 15:26:56 +0100

    ..

--- randomPractise/src/Launcher.java
+++ randomPractise/src/Launcher.java
@@ -1,138 +1,72 @@
 import java.io.*;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.RandomAccess;
+import java.util.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Launcher {
 
-    public ArrayList<String> names;
+    public int[] bubbleSort(int[] numbers) {
 
-
-    public void ls(String path) {
-        File f = new File(path);
-        String[] dirs = {};
-        if (f.isDirectory()) {
-            dirs = f.list();
-        } else
-            System.out.println("File: " + f.getName() + " path: " + f.getPath());
-
-        if (dirs != null)
-            show(dirs);
-
-    }
-
-    public void cp() throws IOException {
-        File f = new File("C:\\Users\\josem\\Desktop\\destino\\gonzalogafasXDXD.txt");
-        File f2 = new File("C:\\Users\\josem\\Desktop\\destino\\kljasdsadf\\" + f.getName());
-
-
-        if (f2.createNewFile())
-            System.out.println("sucess");
-
-    }
-
-    public void show(String[] dirs) {
-        for (String d : dirs) {
-            System.out.println("Filename:" + d);
-        }
-
-    }
-
-    public void countWords() {
-        HashMap<String, Integer> words = new HashMap<String, Integer>();
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader("C:\\Users\\josem\\Desktop\\destino\\yourmom.txt"));
-            String line = "";
-
-            while ((line = bfr.readLine()) != null) {
-                String[] phrase = line.split(" ");
-
-                for (String w : phrase) {
-                    if (!words.containsKey(w))
-                        words.put(w, 1);
-                    else
-                        words.put(w, words.get(w) + 1);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] < numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
                 }
             }
-
-            for (Map.Entry<String, Integer> entry : words.entrySet()) {
-                System.out.println(entry);
-            }
-            System.out.println("---");
-            System.out.println(words.get("la"));
-
-            bfr.close();
-        } catch (Exception e) {
-
         }
-
-
+        return numbers;
     }
 
-    public void createIndex() {
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader("C:\\Users\\josem\\Downloads\\db.csv"));
-            BufferedWriter bfw = new BufferedWriter(new FileWriter("index.csv"));
-
-            String line = bfr.readLine();
-            int counter = line.getBytes().length + 2;
-
-            while ((line = bfr.readLine()) != null) {
-                bfw.write(counter + ";" + line.split(";")[0] + '\n');
-                counter += line.getBytes().length + 2;
-
-            }
-
-            bfr.close();
-            bfw.close();
-        } catch (Exception e) {
+    public int[] integerToArray(int number) {
+        String numberStr = String.valueOf(number);
 
 
+        int[] digits = new int[numberStr.length()];
+
+        for (int i = 0; i < numberStr.length(); i++) {
+            digits[i] = Character.getNumericValue(numberStr.charAt(i));
         }
 
+        return digits;
     }
 
-    public void raf(String dni) {
-        try {
 
-            BufferedReader bfr = new BufferedReader(new FileReader("index.csv"));
-            String line = "";
-            int index = -1;
-
-            while ((line = bfr.readLine()) != null) {
-                if (line.split(";")[1].equalsIgnoreCase(dni))
-                    index = Integer.parseInt(line.split(";")[0]);
-            }
-
-            RandomAccessFile raf = new RandomAccessFile("C:\\Users\\josem\\Downloads\\db.csv", "r");
-            raf.seek(index);
-            System.out.println(raf.readLine());
-            System.out.println(raf.readLine());
-
-
-            bfr.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
+    public void show(int[] numbers) {
+        for (Integer n : numbers) {
+            System.out.println(n);
         }
-
     }
+
+    public int getNumber(int number) {
+        String numString = "";
+        int sorted[] = bubbleSort(integerToArray(number));
+        for (Integer n : sorted) {
+            numString += n + "";
+        }
+        return Integer.parseInt(numString);
+    }
+
+    /*
+
+    public int oppositeNumber(int n) {
+        String number = String.valueOf(n);
+        char[] digits = number.toCharArray();
+        Arrays.sort(digits);
+        StringBuilder newNumber = new StringBuilder(new String(digits));
+        newNumber.reverse();
+
+        return Integer.parseInt(newNumber.toString());
+    }
+
+     */
 
     public static void main(String[] args) throws IOException {
         Launcher l = new Launcher();
-        //l.countWords();
-        //l.createIndex();
-        //l.raf("74639192F");
-
-        double n = 5;
-        int n2 = 6;
-
-        int n3 = (int) n;
-
+        //System.out.println(l.oppositeNumber(239));
+        int[] numbers = {2, 3, 9, 8};
+        System.out.println((l.getNumber(1234)));
     }
 }
