4356f268024666a9b393f997e916cf52ee24132c
Relampag0o <josemariar.l17@gmail.com>
2024-02-24 18:48:53 +0100

    .
    .

--- MongoDBIIQueries/src/main/java/org/example/Main.java
+++ MongoDBIIQueries/src/main/java/org/example/Main.java
@@ -1,19 +1,32 @@
 package org.example;
+import com.mongodb.client.*;
+import org.bson.Document;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public void openConnection(){
+        String uri = "mongodb://localhost:27017";
+        MongoClient mongoClient = MongoClients.create(uri);
+        System.out.println("Connected to the database successfully");
+
+        MongoDatabase database = mongoClient.getDatabase("pokemon");
+        FindIterable<Document> collection = database.getCollection("pokemons").find();
+        for (Document document : collection) {
+            System.out.println(document);
         }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.openConnection();
+
+
     }
 }
